a4f67d8f59be76f9b0508ce9eac54a5c06f02294
znicholasbrown <znicholasbrown@gmail.com>
2019-06-06 15:22:53 -0400

    Adds mongodb database connection info and some basic chore add/list/delete function.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const SlackBot = require('slackbots');
-const util = require('util');
+let mongoose = require('mongoose');
+let util = require('util');
+
+// Slackbot section
+let SlackBot = require('slackbots');
 
 require('dotenv').config({path: __dirname + '/.env'})
 
@@ -20,3 +23,78 @@ bot.on('message', ( message ) => {
         bot.postMessage(message.user, 'meow!', params);
     }
 });
+
+
+// Database Function Section
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+const Schema = mongoose.Schema;
+ 
+const ChoreSchema = new Schema({
+    title: String,
+    instructions: String,
+    difficulty: { type: Number, min: 1, max: 4 },
+    date: { type: Date, default: Date.now },
+    creator: String,
+    frequency: [{ type: Number, default: [0, 1, 2, 3, 4, 5, 6]}],
+    deleted: { type: Boolean, default: false }
+});
+
+const Chore = mongoose.model('chore', ChoreSchema);
+
+const getChores = () => {
+    Chore.find({ deleted: false }, ( err, docs ) => {
+        if (err) {
+            console.log(util.inspect(err));
+            return 400;
+        }
+        console.log(util.inspect(docs));
+        return docs;
+    });
+}
+
+const getDeletedChores = () => {
+    Chore.find({ deleted: true }, ( err, docs ) => {
+        if (err) {
+            console.log(err);
+            return 400;
+        }
+        console.log(util.inspect(docs));
+        return docs;
+    });
+}
+
+const addChore = ( choreData ) => {
+    new Chore(choreData).save( errorCallback );
+}
+
+const deleteChore = ( choreId ) => {
+    Chore.findById( choreId, ( err, chore ) => {
+        if (err) {
+            console.log(util.inspect(err));
+            return 400;
+        }
+
+        chore.deleted = true;
+        chore.save( errorCallback );
+    });
+}
+
+const reinstateDeletedChore = ( choreId ) => {
+    Chore.findById( choreId, ( err, chore ) => {
+        if (err) {
+            console.log(util.inspect(err));
+            return 400;
+        }
+
+        chore.deleted = false;
+        chore.save( errorCallback );
+    });
+}
+
+const errorCallback = ( err ) => {
+    if (err) {
+        console.log(util.inspect(err));
+        return 400;
+    }
+}
