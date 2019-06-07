6b515a5e5146a09314f93a7c4ac1e050edb3ec68
znicholasbrown <znicholasbrown@gmail.com>
2019-06-07 13:56:35 -0400

    Adds methods to access auth2.0 for google api. We can now check today's out of office calendar to retreive a list of the email addresses of those out.

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ process.on( 'SIGTERM', () => {
     })
  });
 
- schedule.scheduleJob('* 10 * * *', (sched) => {
+schedule.scheduleJob('* 10 * * *', (sched) => {
 
     Chore.find({ deleted: false }, ( err, docs ) => {
         if (err) {
@@ -174,3 +174,93 @@ process.on( 'SIGTERM', () => {
     });
 
 })
+
+
+// Google API auth section
+
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from env.
+authorize(listOOOEvents);
+
+function authorize(callback) {
+    const {client_secret, client_id, redirect_uris} = {
+        client_secret: process.env.client_secret, 
+        client_id: process.env.client_id, 
+        redirect_uris: ["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
+    }
+
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+  
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getAccessToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) return console.error(err);
+            console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+        });
+    });
+}
+
+
+function listOOOEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.list({
+      calendarId: process.env.CALENDAR_ID,
+      timeMin: (new Date(new Date().setHours(0,0,0,0))).toISOString(),
+      timeMax: (new Date(new Date().setHours(23,59,59,0))).toISOString(),
+      singleEvents: true,
+      alwaysIncludeEmail: true,
+      orderBy: 'startTime',
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const events = res.data.items;
+      if (events.length) {
+        let ooo = []
+        console.log('Upcoming 10 events:');
+        events.map((event, i) => {
+          const start = event.start.dateTime || event.start.date;
+          console.log(`${start} - ${event.summary}`);
+          if ( !ooo.includes(event.creator.email) ) {
+              ooo.push(event.creator.email)
+          }
+        });
+
+        console.log(ooo.join(', '))
+      } else {
+        console.log('No upcoming events found.');
+      }
+    });
+}
